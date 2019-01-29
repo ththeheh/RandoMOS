af7bef98f0a14195165ea71d5ce0d2fa08a182b3
hxu829 <ABcc2129>
2019-01-29 17:45:46 +1300

    register page

--- web/regjs.js
+++ web/regjs.js
@@ -12,27 +12,9 @@ function cpassword() {
         alert("Your confirmation password does not match your password!");
         redirect("reg.html");
     } else {
+        redirect("login.html")
     }
 }
 
 
-function countrylist(){
-    var myDiv = document.getElementById("countrylist");
-
-//Create array of options to be added
-    var array = ["Volvo","Saab","Mercades","Audi"];
-
-//Create and append select list
-    var selectList = document.createElement("select");
-    selectList.setAttribute("id", "mySelect");
-    myDiv.appendChild(selectList);
-
-//Create and append the options
-    for (var i = 0; i < array.length; i++) {
-        var option = document.createElement("option");
-        option.setAttribute("value", array[i]);
-        option.text = array[i];
-        selectList.appendChild(option);
-    }
-}
 
