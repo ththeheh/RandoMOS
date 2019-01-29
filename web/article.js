b426eee5213c4188f5d8b48668513e5e4fe0ba03
hxu829 <ABcc@129>
2019-01-30 09:48:18 +1300

    article page

--- web/article.js
+++ web/article.js
@@ -1,11 +1,17 @@
 var i = 0;
 
 function addcomment() {
-    var a1 = "<div class='row'><div class='col-lg-8 card dark-grey'><div class='container center'><p>video</p><img src= ";
-    var a = "";            //icon image
-    var a2 = " alt=sir pic style=width:80%><p>John Doe</p></div></div></div></div>";
+    var cm = document.getElementById("comment").value;
+    console.log(cm);
+    var a1 = "<div class='card dark-grey'>";
+    var a2 = "<img class='card-img-top img-thumbnail rounded-circle boarder-primary' src='./images/icons/666201.png' alt='Card image cap' style='width: 50px;height: 50px;'>" //icon image
+    var a3 = "<div class='card-body'> ";
+    var a4 = "<p class='card-title'>  Name of Commentor</p>"
+    var a5 = "<p class='card-text'>";
+    var a6 = cm + "</p></div></div></div><br/><button type='submit' class=\"btn btn-info btn-sm float-right\" onclick=''><strong>Reply</strong></button><br/><br/>";
+
     var div = document.createElement('div');
-    div.innerHTML = a1+a+a2;
+    div.innerHTML = a1+a2+a3+a4+a5+a6;
     document.getElementById("cmList").appendChild(div);
 
 }
